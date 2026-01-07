7a1663ea4fb29303f8c7e0ba248ac2f217a4f1bc
jpereidrovo <josecarlospereidrovo@gmail.com>
2026-01-07 13:35:21 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log("app.js cargado correctamente");
+document.addEventListener("DOMContentLoaded", () => {
+  const el = document.getElementById("jsStatus");
+  if (el) el.textContent = "✅ app.js cargado correctamente";
+});
